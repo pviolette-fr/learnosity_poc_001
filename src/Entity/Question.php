ac935670a672b3517bc66e0b7cc438b68b4295b5
Paulin Violette <pviolette@wiley.com>
2020-01-16 08:55:58 +0000

    Initial Commit

---
+++ src/Entity/Question.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\QuestionRepository")
+ */
+class Question
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $config;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Quiz", inversedBy="questions")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $quiz;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    public function setConfig($config): self
+    {
+        $this->config = $config;
+
+        return $this;
+    }
+
+    public function getQuiz(): ?Quiz
+    {
+        return $this->quiz;
+    }
+
+    public function setQuiz(?Quiz $quiz): self
+    {
+        $this->quiz = $quiz;
+
+        return $this;
+    }
+}
